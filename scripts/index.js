2f2bff725e0cce0b4122bab7d69f455e92be70fe
Isaac Fonseca <isaacfo1010@gmail.com>
2018-10-23 14:26:50 -0400

    added pizza icons

--- scripts/index.js
+++ scripts/index.js
@@ -6,4 +6,28 @@ fetch("https://api.fitbit.com/1/user/-/profile.json",
     }
 }
 )
+.then(j => j.json())
 .then(console.log)
+
+
+const theBody = document.querySelector("body");
+function addPizza() {
+    // creates new images element
+    const newImg = document.createElement("img");
+    // add the pizza icon
+    const newPizza = document.createAttribute("src");
+    newImg.src = "https://png.icons8.com/color/50/000000/pizza.png";
+    theBody.appendChild(newImg);
+};
+
+    for (let i = 0; i < 10; i ++) {
+        addPizza();
+    }
+
+
+
+// addPizza();
+// addPizza();
+// addPizza();
+// addPizza();
+// addPizza();
