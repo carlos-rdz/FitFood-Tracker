320140e98831ba6a8ce59d55a32f562c6eb05703
Collin Argo <collinargo@gmail.com>
2018-10-23 18:02:32 -0400

    remove code to store response values

--- scripts/calories.js
+++ scripts/calories.js
@@ -1,16 +1,17 @@
 // get references to html elements for displaying food data
-const foodName = document.getElementById("food_name");
-const servingSize = document.getElementById("serving_size");
-const servingAmount = document.getElementById("serving_amount");
-const caloriesCount = document.getElementById("calories_count");
-const foodPhoto = document.getElementById("food_photo");
+// const foodName = document.getElementById("food_name");
+// const servingSize = document.getElementById("serving_size");
+// const servingAmount = document.getElementById("serving_amount");
+// const caloriesCount = document.getElementById("calories_count");
+// const foodPhoto = document.getElementById("food_photo");
 
 // create variables to store food terms and result from API call
 const CALORIES_BURNED = 500;
 const FOOD_CHOICES = ['pizza', 'hamburger', 'iceCream', 'fries', 'celery', 'chips', 'candyBar', 'beer', 'taco', 'cupCake']
+// Will replace with html form selection
 const USER_FOOD = FOOD_CHOICES[Math.floor(Math.random() * FOOD_CHOICES.length)];
 const foodResult = {};
-const returnServings = {};
+// const returnServings = {};
 
 function requestFood(calories) {
     fetch(`https://trackapi.nutritionix.com/v2/search/instant?query=${USER_FOOD}&detailed=true&branded=false`,
@@ -22,38 +23,40 @@ function requestFood(calories) {
         }
     })
     .then(convertToJSON)
-    .then(extractFood)
+    // .then(extractFood)
     // .then(drawFood)
-    .then(() => {
+    .then(j => {
+        console.log("calories = " + j.common[0].full_nutrients[4].value);
+        const calories = j.common[0].full_nutrients[4].value;
         const servings = convertCalToNumServings(calories);
-        returnServings.name = foodResult.name;
-        returnServings.servings = servings;
+        foodResult.name = j.common[0].food_name;
+        foodResult.servings = servings;
         // console.log(`${servings} of ${foodResult.name}`);
     });
-    return returnServings;
+    return foodResult;
 }
 
 function convertToJSON(r) {
     return r.json();
 }
 
-function extractFood(j) {
-    const result = j.common[0];
-    const nutrients = result.full_nutrients;
-    const size = result.serving_unit;
-    const amount = result.serving_qty;
-    const pic = result.photo.thumb;
+// function extractFood(j) {
+//     const result = j.common[0];
+//     const nutrients = result.full_nutrients;
+//     const size = result.serving_unit;
+//     const amount = result.serving_qty;
+//     const pic = result.photo.thumb;
     
-    foodResult.name = result.food_name;
-    nutrients.forEach(nutrient => {
-        if (nutrient.attr_id == '208') { // 208 is code for kCal
-            foodResult.calories = nutrient.value;
-        }
-    });
-    foodResult.size = size;
-    foodResult.amount = amount;
-    foodResult.pic = pic;
-}
+//     foodResult.name = result.food_name;
+//     nutrients.forEach(nutrient => {
+//         if (nutrient.attr_id == '208') { // 208 is code for kCal
+//             foodResult.calories = nutrient.value;
+//         }
+//     });
+//     foodResult.size = size;
+//     foodResult.amount = amount;
+//     foodResult.pic = pic;
+// }
 
 // function drawFood() {
 //     foodName.textContent = foodResult.name;
@@ -66,7 +69,7 @@ function extractFood(j) {
 
 function convertCalToNumServings(calories) {
     let servings = 0;
-    while (foodResult.calories * servings < calories) {
+    while (calories * servings < CALORIES_BURNED) {
         servings++;
     }
     return servings;
