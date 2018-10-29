f4bba235a444e2efcc31a81a82fb520653464f56
Collin Argo <collinargo@gmail.com>
2018-10-29 12:57:59 -0400

    add debugging statement

--- scripts/landingPage.js
+++ scripts/landingPage.js
@@ -1,3 +1,4 @@
 const clickLink = document.getElementsByClassName('click-link')[0]
 const thisURL = window.location.host
 clickLink.setAttribute('href', `https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22D4LM&redirect_uri=http%3A%2F%2F${thisURL}%2Ffitbit.html&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800`)
+console.log(thisURL)
